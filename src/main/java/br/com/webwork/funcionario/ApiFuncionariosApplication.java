64b1a123d2b0db1daee9054b85a13bc8c9fcb08e
Thiago Emidio <thiago.augusto.ext@sodexo.com>
2020-08-20 15:43:30 -0300

    Ajustes Travis.yaml

--- src/main/java/br/com/webwork/funcionario/ApiFuncionariosApplication.java
+++ src/main/java/br/com/webwork/funcionario/ApiFuncionariosApplication.java
@@ -4,24 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+
 @CrossOrigin(origins = "*")
 @SpringBootApplication
-public class ApiFuncionariosApplication  {
+public class ApiFuncionariosApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ApiFuncionariosApplication.class, args);
 	}
-
-//	@Override
-//	public void run(String... args) throws Exception {
-//
-//		Logger logger = LoggerFactory.getLogger(ApiFuncionariosApplication.class);
-//
-//		logger.info("----------------------------------------");
-//		logger.info("Configuration properties");
-//		logger.info("   example.username is {}", vaultConfig.getUsername());
-//		logger.info("   example.password is {}", vaultConfig.getPassword());
-//		logger.info("----------------------------------------");
-//
-//	}
 }
--- src/main/java/br/com/webwork/funcionario/vault/configuration/VaultConfig.java
+++ src/main/java/br/com/webwork/funcionario/vault/configuration/VaultConfig.java
@@ -1,13 +1,16 @@
 package br.com.webwork.funcionario.vault.configuration;
 
-//@Data
-//@Configuration
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Data;
+
+@Data
+@Configuration
 public class VaultConfig {
 	
-//	@Value("${username}")
-//	private String username;
-//	
-//	@Value("${password}")
-//	private String password;
+	private String excited;
+	
+	private String foo;
 	
 }
